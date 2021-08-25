ac54520352a225779e37323b68d88f7f82867b37
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-08-25 23:07:27 +0545

    Added Images and UID section

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,6 +1,6 @@
 import 'package:dashboard_ui/app_bars.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'shape.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,6 +16,36 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Stack(
           children: [
             Shape(),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/pp.jpg'),
+                    ),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Sandesh Rimal',
+                      style: GoogleFonts.lato(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      '02-567334',
+                      style: GoogleFonts.lato(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white60),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ));
   }
