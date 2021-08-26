9c06408c4ae500aa61fb4c7a5fcc2de0265a8c38
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-08-26 22:00:17 +0545

    Added: Cards

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -22,47 +22,130 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          Column(
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Container(
+                    height: 65,
+                    margin: EdgeInsets.only(bottom: 30),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0, top: 5.0),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: AssetImage('assets/pp.jpg'),
+                            radius: 25,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 20.0, top: 5.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Sandesh Rimal',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '01-452238',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white60,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          GridView.count(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            primary: false,
             children: [
-              Container(
-                height: 65,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0, top: 5.0),
-                  child: Row(
+              Card(
+                color: Colors.yellow,
+                child: InkWell(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CircleAvatar(
-                        backgroundImage: AssetImage('assets/pp.jpg'),
-                        radius: 25,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20.0, top: 5.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              'Sandesh Rimal',
-                              style: GoogleFonts.lato(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              '01-452238',
-                              style: GoogleFonts.lato(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white60,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                      Icon(Icons.portrait_rounded, size: 100),
+                      Text('Profile')
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: InkWell(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.portrait_rounded, size: 100),
+                      Text('Profile')
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: InkWell(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.portrait_rounded, size: 100),
+                      Text('Profile')
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: InkWell(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.portrait_rounded, size: 100),
+                      Text('Profile')
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: InkWell(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.portrait_rounded, size: 100),
+                      Text('Profile')
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                child: InkWell(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.portrait_rounded, size: 100),
+                      Text('Profile')
                     ],
                   ),
                 ),
               ),
             ],
-          ),
+          )
         ],
       ),
     );
